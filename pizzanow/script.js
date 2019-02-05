a6874b24baf4d9ce82b1ad67a50b8229fb31e193
Alfonso Dumlao <alfonsodumlao@ool-ad020051.dyn.optonline.net>
2019-02-05 12:37:17 -0500

    got the game to lose when enemy reaches threshhold, and clear enemy and clearinterval once enemy is removed so you wont keep losing

--- pizzanow/script.js
+++ pizzanow/script.js
@@ -4,61 +4,76 @@
 //c2 will move up 5px every 3seconds
 //c3 will move up 5px every 8seconds
 
-const customer = document.querySelector('.middle')
+
+const path1 = document.querySelector('.path1')
+const path2 = document.querySelector('.path2')
+const path3 = document.querySelector('.path3')
 
 let c1 = document.createElement('div')
 let c2 = document.createElement('div')
 let c3 = document.createElement('div')
 
+let createEnemy = () => {
+  c1.classList.add('c1')
+  path1.appendChild(c1);
+
+  c2.classList.add('c2')
+  path2.appendChild(c2);
+
+  c3.classList.add('c3')
+  path3.appendChild(c3);
+};
+
 let i = 0;
 let j= 0;
 let k =0;
 
-const lost = () => {
-  if (i > 240 || j > 240 || k > 240){
+const moveEnemy = () => {
+  c1.style.top = `${i}px`;
+  i += 10;
+
+  c2.style.top = `${j}px`;
+  j += 5;
+
+  c3.style.top = `${k}px`;
+  k += 15;
+};
+
+//one set setInterval(function () {
+//update position of enemies and check for Lost
+//keep an object of bad guys
+//if killed, take out of dom and remove from array of objects
+const interval = () => {
+  c1.style.top = `${i}px`;
+  i += 10;
+  c2.style.top = `${j}px`;
+  j += 5;
+  c3.style.top = `${k}px`;
+  k += 15;
+
+  if (c1.style.top === `230px`){
     console.log('You Lost!')
-  };
+    clearInterval(thisInterval);
+  } else if (c2.style.top === `235px`){
+     console.log('You Lost!')
+     clearInterval(thisInterval);
+  } else if (c3.style.top === `240px`){
+    console.log('You Lost!')
+    clearInterval(thisInterval);
+   };
 };
 
-let enemy = () => {
-  c1.classList.add('c1')
-  customer.appendChild(c1);
+let thisInterval = setInterval(interval, 1000);
 
-  c2.classList.add('c2')
-  customer.appendChild(c2);
-
-  c3.classList.add('c3')
-  customer.appendChild(c3);
-
-  const startTop = () => {
-        c1.style.top = 0 + 'px';
-        c2.style.top = 0 + 'px';
-        c3.style.top = 0 + 'px';
-      };
-
-  const moveDown1 = () => {
-    c1.style.top = `${i}px`;
-    i += 30;
-    lost();
-    };
-  const moveDown2 = () => {
-    c2.style.top = `${j}px`;
-    j += 30;
-    lost();
-    };
-  const moveDown3 = () => {
-    c3.style.top = `${k}px`;
-    k += 30;
-    lost();
-    };
-
-   startTop();
-   setInterval(moveDown1, 1000);
-   setInterval(moveDown2, 3000);
-   setInterval(moveDown3, 1500);
+const checkLost = () => {
+  if (i > 240){
+    console.log('You Lost!')
+  } else if (j > 240){
+     console.log('You Lost!')
+  } else if (k > 240){
+    console.log('You Lost!')
+   };
 };
-
-setTimeout(enemy, 1000);
 
 //defense
 //add event listeners to .player
@@ -70,16 +85,16 @@ setTimeout(enemy, 1000);
 //set timeout so after 1 second, .served disappears
 
 const player = document.querySelector('.player');
+
 const body = document.querySelector('body');
 const s1 = document.querySelector('#s1');
 const s2 = document.querySelector('#s2');
 const s3 = document.querySelector('#s3');
+
 move = 30;
 
 const movePlayer = () => {
   const x = event.keyCode;
-  let served = document.createElement('div');
-  served.classList.add('served');
 
   if (x===37){
     player.style.left = `${move}px`;
@@ -96,25 +111,35 @@ const movePlayer = () => {
      move = 840;
      };
    };
+};
 
-   if (x === 32 && move >= 0 && move < 70){
+const playerAttack = () => {
+    const y = event.keyCode;
+
+    let served = document.createElement('div');
+    served.classList.add('served');
+
+   if (y === 32 && move >= 0 && move < 70){
      s1.appendChild(served);
      c1.classList.add('dead');
      setTimeout(() => {
-       c1.remove();
+       path1.removeChild(c1);
      }, 1000);
-   } else if (x === 32 && move > 380 && move < 460){
+     clearInterval(thisInterval);
+   } else if (y === 32 && move > 380 && move < 460){
       s2.appendChild(served);
-      c2.classList.add('dead');
+      let c2Dead = c2.classList.add('dead');
       setTimeout(() => {
-        c2.remove();
+        path2.removeChild(c2);
       }, 1000);
-   } else if (move > 740 && move < 860 && x === 32){
+      clearInterval(thisInterval);
+   } else if (move > 740 && move < 860 && y === 32){
      s3.appendChild(served);
-     c3.classList.add('dead');
+     let c3Dead = c3.classList.add('dead');
      setTimeout(() => {
-       c3.remove();
+       path3.removeChild(c3);
      }, 1000);
+       clearInterval(thisInterval);
    }
 
    setTimeout(() => {
@@ -123,8 +148,15 @@ const movePlayer = () => {
 
 };
 
-body.addEventListener('keyup', movePlayer);
+const playGame = () => {
+  createEnemy();
+  moveEnemy();
+  body.addEventListener('keyup', movePlayer);
+  body.addEventListener('keyup', playerAttack);
 
+};
+
+playGame();
 
 //win LOSE
 //DONE if there is an enemy- enemy will change image and disapper.
